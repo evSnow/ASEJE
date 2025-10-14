9c50c378405dac08ae38d49763218223604a2d94
unt-sa2044 <SulavAdhikari@my.unt.edu>
2025-10-13 22:26:30 -0500

    Created UIHelper.js

---
+++ source/UIHelper.js
@@ -0,0 +1,25 @@
+const vscode = require('vscode');
+
+/**
+ * Registers commands and logic related to UI assistance, such as the Walkthrough.
+ * This function is called from extension.js.
+ * @param {vscode.ExtensionContext} context
+ */
+function registerUIHelperCommands(context) {
+    
+    const walkthroughDisposable = vscode.commands.registerCommand('aseje.showWalkthrough', () => {
+        showGuidedWalkthrough();
+    });
+    context.subscriptions.push(walkthroughDisposable);
+}
+
+function showGuidedWalkthrough() {
+    const walkthroughId = 'aseje.myFirstWalkthrough'; 
+    
+    vscode.commands.executeCommand('workbench.action.openWalkthrough', walkthroughId);
+    vscode.window.showInformationMessage('Opening ASEJE Guided Walkthrough...');
+}
+
+module.exports = {
+    registerUIHelperCommands
+}
