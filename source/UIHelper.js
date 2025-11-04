65ae07acc82f96b114560109c257a79593e75f82
ethanbone9 <ethanbone9@gmail.com>
2025-11-03 18:28:35 -0600

    Added walkthorugh.html along with some changes to UIHelper.js

--- source/UIHelper.js
+++ source/UIHelper.js
@@ -1,26 +1,44 @@
+// source/UIHelper.js
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
 /**
- * Registers commands and logic related to UI assistance, such as the Walkthrough.
- * This function is called from extension.js.
- * @param {vscode.ExtensionContext} context
+ * Registers walkthrough and help panel commands
  */
 function registerUIHelperCommands(context) {
-    
-    const walkthroughDisposable = vscode.commands.registerCommand('aseje.showWalkthrough', () => {
-        showGuidedWalkthrough();
-    });
-    context.subscriptions.push(walkthroughDisposable);
+  // Existing walkthrough command
+  const walkthroughDisposable = vscode.commands.registerCommand('aseje.showWalkthrough', () => {
+    showGuidedWalkthrough(context);
+  });
+
+  context.subscriptions.push(walkthroughDisposable);
 }
 
-function showGuidedWalkthrough() {
-    const walkthroughId = 'aseje.myFirstWalkthrough'; 
-    
-    vscode.commands.executeCommand('workbench.action.openWalkthrough', walkthroughId);
-    vscode.window.showInformationMessage('Opening ASEJE Guided Walkthrough...');
+function showGuidedWalkthrough(context) {
+  const panel = vscode.window.createWebviewPanel(
+    'asejeWalkthrough',
+    'Welcome to ASEJE',
+    vscode.ViewColumn.One,
+    { enableScripts: true }
+  );
+
+  const htmlPath = path.join(context.extensionPath, 'view', 'walkthrough.html');
+  const html = fs.readFileSync(htmlPath, 'utf8');
+  panel.webview.html = html;
+
+  // Handle button clicks from HTML
+  panel.webview.onDidReceiveMessage(async (message) => {
+    if (message.command === 'createProject') {
+      vscode.commands.executeCommand('aseje.createStarterProject');
+    }
+    if (message.command === 'openTextSettings') {
+      vscode.commands.executeCommand('aseje.textSetting');
+    }
+  });
 }
 
 module.exports = {
-    registerUIHelperCommands,
-    showGuidedWalkthrough
-}
+  registerUIHelperCommands,
+  showGuidedWalkthrough,
+};
