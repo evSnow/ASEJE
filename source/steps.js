b163aa7a177dcc4ab91f01a218e4570e79d5d3bd
evSnow <evanrodriguez2022@gmail.com>
2025-11-08 17:52:46 -0600

    Added button and improve setting loding speed and change font layout

--- source/steps.js
+++ source/steps.js
@@ -10,13 +10,26 @@ const stepOneDisposable = vscode.commands.registerCommand('aseje.stepOne', () =>
     'step one',
     'first lesson',
     vscode.ViewColumn.One,
-    { enableScripts: true }
+    { enableScripts: true,
+      retainContextWhenHidden: true  
+     }
   );
 
   const htmlPath = path.join(context.extensionPath, 'view', 'firstLesson.html');
-  const html = fs.readFileSync(htmlPath, 'utf8');
 
+  let html = fs.readFileSync(htmlPath, 'utf8');
+  const config = vscode.workspace.getConfiguration();
+  let fontSize = (config.get('aseje.fontSize') || 14);  
+  console.log(fontSize)
+  html=html.replace(/(\d+)px/,`${fontSize}px`);
+  console.log(html)
   panel.webview.html = html;
+    panel.webview.onDidReceiveMessage(async (message) => {
+    console.log(message.command)
+    if (message.command === 'go_Home') {
+        vscode.commands.executeCommand('aseje.showWalkthrough');
+    }
+  });
 });
 context.subscriptions.push(stepOneDisposable);
 }
