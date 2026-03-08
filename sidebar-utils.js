642c700f3da215f1ad537f4e63e171ac7c780370
anujjyakhwa <anuj.jyakhwa@gmail.com>
2026-03-07 23:16:03 -0600

    Add utility functions for ASEJE Sidebar

---
+++ sidebar-utils.js
@@ -0,0 +1,14 @@
+// Utility functions for ASEJE Sidebar
+
+function getSidebarWelcomeMessage() {
+    return "Welcome to ASEJE Sidebar Tools";
+}
+
+function getSidebarDescription() {
+    return "Quick access to references, debugging tools, and beginner features.";
+}
+
+module.exports = {
+    getSidebarWelcomeMessage,
+    getSidebarDescription
+};
